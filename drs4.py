39e3d87bc4ab07e0051e4f7207d9e22f5de2bf34
cta <cta@CTA-Macmini.local>
2023-12-27 01:37:59 +0900

    [Fix] no Progres bar func

--- drs4.py
+++ drs4.py
@@ -16,6 +16,16 @@ def RunHageFusaScript2(logfile, file, tree, eventnum, serial=2386, delay = 0.0, 
     except:
         print("ERROR with DRS4!")
 
+    command = ["tail", "-n", "3", logfile, "|", "tac", "|", "head", "-n", "1"]
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output_stdout = proc.stdout.read() # 標準出力の場合
+    output_stderr = proc.stderr.read() # 標準エラー出力の場合
+    #print(output_stdout)
+    if "DAQ end" in output_stdout.decode():
+        print(Color.GREEN + "END DAQ!" + Color.RESET)
+    else:
+        print(Color.RED + "DAQ is not end correctly...." + Color.RESET)
+
 def RunHageFusaScript(logfile, file, tree, eventnum, serial=2386, delay = 0.0, freq=5.0):
     READDrs4_path = '/Users/cta/Software/JustReadDrs4_Sunada_20210408/ReadDrs4'
 
@@ -38,7 +48,7 @@ def RunHageFusaScript(logfile, file, tree, eventnum, serial=2386, delay = 0.0, f
     thread1.start()
     thread2.start()
     thread1.join()
-    thread2.join(0)     #ReadDRS4がおわったら強制的に落ちる
+    thread2.join()     #ReadDRS4がおわったら強制的に落ちる
 
     command = ["tail", "-n", "3", logfile, "|", "tac", "|", "head", "-n", "1"]
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
