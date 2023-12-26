4c4d302054c354e87b2af1f2a284cc561549ef53
cta <cta@CTA-Macmini.local>
2023-12-26 16:26:36 +0900

    [Fix] change mode of multiprocess

--- drs4.py
+++ drs4.py
@@ -38,7 +38,7 @@ def RunHageFusaScript(logfile, file, tree, eventnum, serial=2386, delay = 0.0, f
     thread1.start()
     thread2.start()
     thread1.join()
-    thread2.join()
+    thread2.join(0)     #ReadDRS4がおわったら強制的に落ちる
 
     command = ["tail", "-n", "3", logfile, "|", "tac", "|", "head", "-n", "1"]
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
